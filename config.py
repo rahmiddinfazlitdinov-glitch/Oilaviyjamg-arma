85c3e37759a20400f8a28185a527beaa97cb693b
rahmiddinfazlitdinov-glitch <rahmiddinfazlitdinov@gmail.com>
2026-09-06 21:23:45 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from datetime import time
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "SIZNING_BOT_TOKENINGIZ_BU_YERGA")
+
+ADMIN_IDS = [
+    111111111,
+]
+
+DAILY_CHECK_TIME = time(hour=4, minute=0)
