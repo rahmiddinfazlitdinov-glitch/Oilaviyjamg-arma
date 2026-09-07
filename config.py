ebef86c037a16787e58d9654acd3b7eafe4a5a8a
rahmiddinfazlitdinov-glitch <rahmiddinfazlitdinov@gmail.com>
2026-09-07 15:07:08 +0500

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from datetime import time
 BOT_TOKEN = os.getenv("BOT_TOKEN", "SIZNING_BOT_TOKENINGIZ_BU_YERGA").strip()
  
 ADMIN_IDS = [
-    7322817894,
+    7322817894,2053056622
 ]
  
 DAILY_CHECK_TIME = time(hour=4, minute=0)
